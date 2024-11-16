65bf4144cbb7b26a10bc5d8478d6689bf3af1106
just-ting <p76131220@gs.ncku.edu.tw>
2024-11-17 05:25:41 +0800

    Update matmul.s

--- src/matmul.s
+++ src/matmul.s
@@ -116,15 +116,11 @@ inner_loop_start:
     
 inner_loop_end:
     # TODO: Add your own implementation
-	lw ra, 0(sp)
-    lw s0, 4(sp)
-    lw s1, 8(sp)
-    lw s2, 12(sp)
-    lw s3, 16(sp)
-    lw s4, 20(sp)
-    lw s5, 24(sp)
-	addi sp, sp, 28
-	ret
+	addi s0, s0, 1       
+    slli t1, a2, 2      
+    add s3, s3, t1      
+    j outer_loop_start
+
 outer_loop_end:
     lw ra, 0(sp)
     lw s0, 4(sp)
